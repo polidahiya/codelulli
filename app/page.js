ad84f4ebe1e2dca5a3b83ed89143c7920ca22e7a
parvesh <polidahiya830@gmail.com>
2025-07-16 11:29:26 +0530

    .

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Programmingpages from "./(programming)/_comps/Morepages";
 
 function page() {
-  return <div></div>;
+  return (
+    <div>
+      <Programmingpages />
+    </div>
+  );
 }
 
 export default page;
